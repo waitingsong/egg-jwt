dabed72bc99915d0fea832d5cfdb03a2f2a06df8
waiting <1661926154@qq.com>
2019-08-25 19:54:03 +0800

    test: middleware passthrough:function->true

--- test/52.middleware.exception.passthrough.test.ts
+++ test/52.middleware.exception.passthrough.test.ts
@@ -7,6 +7,7 @@ import { parseConfig } from '../src/lib/util'
 
 import { token1 } from './test.config'
 import { createContext, createNextCb } from './test.util'
+import { Context } from 'egg';
 
 
 const filename = basename(__filename)
@@ -83,5 +84,78 @@ describe(filename, () => {
       await mw(ctx, next)
     })
   })
+
+
+  describe('Should middleware works with passthrough:function->true', () => {
+    it('w/o token', async () => {
+      const ctx = createContext()
+      const next = createNextCb(ctx, JwtMsg.TokenNotFound)
+      const config: JwtConfig = parseConfig(initialConfig)
+      config.client.authOpts = {
+        cookie: false,
+        key: 'user',
+        passthrough: passthroughCbTrue,
+      }
+      const mw = jwtMiddlewareFactorey(config)
+
+      await mw(ctx, next)
+    })
+    it('w/o token debug', async () => {
+      const ctx = createContext()
+      const next = createNextCb(ctx, JwtMsg.TokenNotFound)
+      const config: JwtConfig = parseConfig(initialConfig)
+      config.client.authOpts = {
+        cookie: false,
+        key: 'user',
+        passthrough: passthroughCbTrue,
+      }
+      config.client.debug = true
+      const mw = jwtMiddlewareFactorey(config)
+
+      await mw(ctx, next)
+    })
+
+    it('w/o secret', async () => {
+      const props = {
+        header: {
+          authorization: `${schemePrefix} ${token1}`,
+        },
+      }
+      const ctx = createContext(props)
+      const next = createNextCb(ctx, JwtMsg.TokenValidFailed)
+      const config: JwtConfig = parseConfig(initialConfig)
+      config.client.authOpts = {
+        cookie: false,
+        key: 'user',
+        passthrough: passthroughCbTrue,
+      }
+      const mw = jwtMiddlewareFactorey(config)
+
+      await mw(ctx, next)
+    })
+    it('w/o secret debug', async () => {
+      const props = {
+        header: {
+          authorization: `${schemePrefix} ${token1}`,
+        },
+      }
+      const ctx = createContext(props)
+      const next = createNextCb(ctx, JwtMsg.TokenValidFailed)
+      const config: JwtConfig = parseConfig(initialConfig)
+      config.client.authOpts = {
+        cookie: false,
+        key: 'user',
+        passthrough: passthroughCbTrue,
+      }
+      config.client.debug = true
+      const mw = jwtMiddlewareFactorey(config)
+
+      await mw(ctx, next)
+    })
+  })
 })
 
+
+async function passthroughCbTrue(_ctx: Context): Promise<boolean> {
+  return true
+}
