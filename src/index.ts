f0ce3d28b7ef5e2b76f5964ceffd39013d75663d
waiting <1661926154@qq.com>
2019-08-25 20:03:11 +0800

    feat: export
    - ./lib/resolvers
    - ./lib/util
    - jwtMiddlewareFactorey from ./app/middleware/jwt

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 
+import jwtMiddlewareFactorey from './app/middleware/jwt'
+
+
 export * from './lib/config'
 export * from './lib/jwt'
 export * from './lib/model'
+export * from './lib/resolvers'
+export * from './lib/util'
+export { jwtMiddlewareFactorey }
+
