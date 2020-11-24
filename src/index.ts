594f1e82b7976908a4daf37869fb252ea8db4089
waiting <1661926154@qq.com>
2020-11-24 21:45:16 +0800

    refactor: build target structure

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,6 @@
 import jwtMiddlewareFactorey from './app/middleware/jwt'
 
 
-export * from './lib/config'
-export * from './lib/jwt'
-export * from './lib/model'
-export * from './lib/resolvers'
-export * from './lib/util'
+export * from './lib/index'
 export { jwtMiddlewareFactorey }
 
---
+++ src/lib/index.ts
@@ -0,0 +1,7 @@
+
+export * from './config'
+export * from './jwt'
+export * from './model'
+export * from './resolvers'
+export * from './util'
+
